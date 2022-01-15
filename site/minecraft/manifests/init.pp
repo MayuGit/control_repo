9ffff26955afd9c9c573feef0c842046fbf3af7f
MayuGit <vicky.pardeshi@gmail.com>
2022-01-15 23:39:06 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -18,4 +18,9 @@ class minecraft {
       ensure => file,
       source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  
+  service {'minecraft',
+    ensure => running,
+    enable => true,
+  }
 }
