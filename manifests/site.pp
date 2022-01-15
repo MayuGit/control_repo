806af8c8b677e1955dc3f12b86a89c30ba264e1b
MayuGit <vicky.pardeshi@gmail.com>
2022-01-15 18:49:23 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'this is a readme',
-    owner   => 'root',
-  }
-  
-  file {'/root/README':
-    owner => 'root',
-  }
+
 }
